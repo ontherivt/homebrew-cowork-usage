a4dff804e413f4f22e85382c61f024f4c90bf0c3
PJ Hoberman <pj.hoberman@gmail.com>
2026-05-07 11:40:59 -0600

    fix sha256

--- Formula/cowork-usage.rb
+++ Formula/cowork-usage.rb
@@ -7,7 +7,7 @@ class CoworkUsage < Formula
   # together every time you cut a new release.
   url "https://github.com/ontherivt/homebrew-cowork-usage/archive/refs/tags/v0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "207ac94cf57d4623ff9e0f49b9c48f455dd8176447fd75ef9d7127d33e23083c"
 
   license "MIT"
 
